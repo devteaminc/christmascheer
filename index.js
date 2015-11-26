ad33c2cdf650108bc8340e00224989b91aaec488
Rob Mills <robjmills@gmail.com>
2015-11-26 17:26:18 +0000

    generate an average score based on overall ratings

--- index.js
+++ index.js
@@ -29,18 +29,39 @@ app.get(/^(.+)$/, function(req, res) {
 io.on('connection', function (socket){
 
   // array of terms to trackList
-  var trackList = 'christmas';
+  var trackList = 'thanksgiving';
+  var lastTweetId = null;
+  var totalTweets = 0;
+  var totalScore = 0;
+  var adjustedScore = 0;
 
   // open stream to Twitter
   client.stream('statuses/filter', {track: trackList}, function(stream) {
     stream.on('data', function(tweet) {
-      var tweetSentiment = sentiment(tweet.text);
 
-      // emit streamed results to frontend socket
-      io.emit('stream',{
-        tweet: tweet,
-        sentiment: tweetSentiment
-      });
+      if(lastTweetId === null){
+        lastTweetId = tweet.id_str;
+      }
+
+      if(tweet.id_str !== lastTweetId){
+
+        var tweetSentiment = sentiment(tweet.text);
+        lastTweetId = tweet.id_str;
+        totalTweets += 1;
+        totalScore += tweetSentiment.score;
+        adjustedScore = parseFloat((totalScore / totalTweets)).toFixed(2);
+
+        console.log("score: " + tweetSentiment.score);
+        console.log("total: " +adjustedScore);
+
+        // emit streamed results to frontend socket
+        io.emit('stream',{
+          tweet: tweet,
+          sentiment: adjustedScore
+        });
+
+      } 
+
     });
    
     stream.on('error', function(error) {
